bef95f1395203b4272e39971c9efe961c120c491
Nicolas Vergez <nicolas.vergez@epitech.eu>
2021-02-14 01:57:25 +0100

    feat(front/api): add responsive bot logs

--- ai/server/controller.js
+++ ai/server/controller.js
@@ -12,7 +12,7 @@ controller.logs = (req, res, next) => {
     try {
         var data = fs.readFileSync('./logs/dump.log', {encoding:'utf8', flag:'r'})
         data = data.split('\n')
-        data = data.slice(Math.max(data.length - 20, 0)).reverse()
+        data = data.slice(Math.max(data.length - 50, 0)).reverse()
         res.send(data)
         return next()
     } catch (e) {
--- front/src/Dashboard/Bot/LogList.js
+++ front/src/Dashboard/Bot/LogList.js
@@ -1,51 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles'
+import PropTypes from 'prop-types';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import { useStyles } from '../Style'
-import raw from '../../logs/dump.log';
+import { fetchLogs } from '../../api/ai'
 
-var data = [
-    "The pair NMRBTC increases by 11.194968553459116% the last 15 seconds. (2021-02-10T17:40:03.395Z) (0.00088400)",
-    "The pair CTSIBTC increases by 11.363636363636353% the last 15 seconds. (2021-02-10T20:37:03.294Z) (0.00000882)",
-    "The pair CTSIBTC increases by 11.34663341645885% the last 15 seconds. (2021-02-10T20:37:03.495Z) (0.00000893)",
-]
+const styles = theme => ({
+    item: {
+        padding: 0
+    }
+})
 
-export default function LogList() {
-    const classes = useStyles();
-    const [logs, setLogs] = React.useState(data)
-/*
-    fetch(raw)
-        .then(r => r.text())
-        .then(text => {
-            const tmp = text.split("\n")
-            setLogs(tmp.slice(Math.max(tmp.length - 20, 0)).reverse())
-        })
-*/
-    /*
-    setInterval(() => {
-        fetch(raw)
-        .then(r => r.text())
-        .then(text => {
-            const tmp = text.split("\n")
-            setLogs(tmp.slice(Math.max(tmp.length - 20, 0)))
-        })
-    }, 5000)
-    */
+class LogList extends Component {
+    _isMounted = false
 
-    return (
-    <div>
-        <Typography variant="h6" className={classes.title}>
-            Logs
-        </Typography>
-        <List >
-            {logs.map((elem) => (
-            <ListItem className={classes.item}>
-                <ListItemText primary={elem}/>
-            </ListItem>
-            ))}
-        </List>
-    </div>
-    )
+    constructor() {
+        super()
+
+        this.state = {
+            logs: []
+        }
+
+        this.handleReload = this.handleReload.bind(this)
+    }
+
+    componentDidMount() {
+        this._isMounted = true
+        this.handleReload()
+        setInterval(this.handleReload, 5000)
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false
+    }
+
+    handleReload() {
+        if (this._isMounted) {
+            fetchLogs()
+            .then((response) => {
+                var tmp = []
+                var i = 1
+                for (var item of response) {
+                    tmp.push({id: i, msg: item})
+                    i += 1
+                }
+                this.setState({logs: tmp})
+            })
+            .catch((error) => {
+                this.setState({logs: []})
+            })
+        }
+    }
+
+    render() {
+        const { classes } = this.props
+        return (
+            <div>
+                <div>
+                    <Typography variant="h6">
+                        Logs
+                    </Typography>
+                </div>
+                <List >
+                    {this.state.logs.map((elem) => (
+                    <ListItem className={classes.item} key={elem.id}>
+                        <ListItemText primary={elem.msg}/>
+                    </ListItem>
+                    ))}
+                </List>
+            </div>
+        )
+    }
 }
+
+LogList.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(LogList)
