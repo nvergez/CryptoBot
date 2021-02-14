ebf4f544e9eb6951b4b8a1bd60fee890d54c089d
Nicolas Vergez <nicolas.vergez@epitech.eu>
2021-02-14 02:47:00 +0100

    feat(ai): Added signals bot

---
+++ ai/README.md
@@ -0,0 +1,9 @@
+# AI
+
+## Run
+Docker compose use server for share logs
+Use bot rules to run bots
+
+
+## Author
+- Nicolas Vergez
---
+++ ai/bots/signals.js
@@ -0,0 +1,41 @@
+import Binance from 'binance-api-node'
+import fs from 'fs'
+
+const client = Binance.default()
+
+var data = []
+var callBySeconds = 5
+var interval = 10
+
+
+var fetchPrices = () => {
+    try {
+        client.prices()
+        .then((response) => {
+                for (var attr in response) {
+                    if (!attr.includes('BTC', 1)) {
+                        delete response[attr]
+                    }
+                }
+                if (data.length >= callBySeconds * interval) {
+                    var front = data.shift()
+                    for (var attr in front) {
+                        var percent = ((response[attr] / front[attr]) - 1) * 100
+                        if (percent >= 1 && response[attr] > 0.00000010) {
+                            var file = fs.createWriteStream("./logs/dump.log", {flags: 'a'})
+                            file.write("The pair " + attr + " increases by " + percent +"% the last 15 seconds. (" + (new Date().toISOString()) + ") (" + response[attr] + ")\n")
+                            file.end()
+                        }
+                    }
+                }
+                data.push(response)
+        })
+        .catch((error) => {
+                console.log(error)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+setInterval(fetchPrices, 1000 / callBySeconds)
